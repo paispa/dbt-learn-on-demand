d4c17b842ff993aac8772569ed7b71427c9049c1
prashanth.pais@t-mobile.com <prashanth.pais@t-mobile.com>
2021-07-14 20:04:46 +0000

    completed macros module

--- models/staging/jaffle_shop/stg_payments.sql
+++ models/staging/jaffle_shop/stg_payments.sql
@@ -5,7 +5,8 @@ with payments as (
         orderid as order_id,
         paymentmethod as payment_method,
         status,
-        amount /100 as amount,
+    -- amount is stored in cents, convert it to dollars
+    {{ cents_to_dollars('amount', 4) }} as amount,
         created as created_at
 
     from {{ source ('stripe','payment') }}
